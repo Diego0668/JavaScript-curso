c91a526b99bfb49877a64693c38918bf2b0c1e24
Diego Augusto <80727709+Diego0668@users.noreply.github.com>
2023-03-05 12:42:04 -0300

    update

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -1,1 +1,18 @@
-alert("Olá")
+function verificar() {
+    //window.alert(`FUNCIONOU`)
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById(`idade`)
+    var res = document.getElementById(`res`)
+
+
+    if (fano.value.length == 1 || fano.value > ano || fano.value.length == 0) {
+        window.alert(`Dados incorretos e tente novamente`)
+    } else {
+        var sexo = document.getElementsByName(`radsex`)
+        var idade = ano - Number(fano.value)
+       // res.innerHTML = (`<p>Minha idade calculada é <strong>${idade}</strong></p>`)
+    }
+
+}
+
