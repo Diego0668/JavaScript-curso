e15d475cf57616c5ef7f773701433f2a2c294758
Diego Augusto <80727709+Diego0668@users.noreply.github.com>
2023-03-06 17:16:52 -0300

    update
    Verificador de Idade bem simples usando o javascript

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -11,7 +11,36 @@ function verificar() {
     } else {
         var sexo = document.getElementsByName(`radsex`)
         var idade = ano - Number(fano.value)
-       // res.innerHTML = (`<p>Minha idade calculada é <strong>${idade}</strong></p>`)
+        var genero = ''
+        var img = document.getElementById(`foto`)
+
+        if (sexo[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute("src", "foto-bebe-f.png")
+            } else if (idade < 21) {
+                img.setAttribute("src", "foto-jovem-f.png")
+            } else if (idade < 50) {
+                img.setAttribute("src", "foto-adulto-f.png")
+            } else {
+                img.setAttribute("src", "foto-idoso-f.png")
+            }
+        } else if (sexo[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute("src", "foto-bebe-m.png")
+            } else if (idade <= 18) {
+                img.setAttribute("src", "foto-jovem-m.png")
+            } else if (idade <= 50) {
+                img.setAttribute("src", "foto-adulto-m.png")
+            } else {
+                img.setAttribute("src", "foto-idoso-m.png")
+            }
+        }
+        res.style.textAlign = 'center'
+        img.style.textAlign = 'center'
+        res.innerHTML = (`<p>Sua idade calculada é <strong>${idade}</strong> anos </p> e detectamos que você é <strong>${genero}</strong>`)
+        res.appendChild(img) //elemento pra aparecer a imagem
     }
 
 }
